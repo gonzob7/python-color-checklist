655aac9593b5ab513c89df6f582eafcd759a194a
Gonzalo Birrueta <silentcr7@gmail.com>
2019-08-28 20:18:24 -0700

    added TESTING function

--- checklist.py
+++ checklist.py
@@ -1,5 +1,33 @@
 checklist = list()
-checklist.append("Blue")
-print(checklist)
-checklist.append("Orange")
-print(checklist)
+
+#CREATE
+def create(item):
+    checklist.append(item)
+
+#READ
+def read(index):
+    return checklist[index]
+
+#UPDATE
+def update(index, item):
+    checklist[index]
+
+#DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+#TESTING
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+test()
